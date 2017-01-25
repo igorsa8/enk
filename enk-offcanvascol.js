e1d321ccfe7c1bd2426b47c084ffd12b29facc04
igorsa8 <igorsa8@gmail.com>
2017-01-25 00:45:37 +0000

    Update enk-offcanvascol.js

--- enk-offcanvascol.js
+++ enk-offcanvascol.js
@@ -1,62 +1,73 @@
 /* USAGE
 
-<button type="button" data-toggle="offcanvas" data-target="#contentX" data-source="#sidebarX" data-canvas="body">
-        Open/Close
+<button type="button" data-toggle="enkoffcanvas" data-target="#content" data-source="#sidebar" data-canvas="#p1">
+        Panel 1
+    </button>
+    
+    <button type="button" data-toggle="enkoffcanvas" data-target="#content" data-source="#sidebar" data-canvas="#p2">
+        Panel 2
     </button>
 
-    <div class="row" id="row-main">
-        <div class="col-md-12" id="contentX">
+<div class="row enk-offcanvas-container" id="row-main">
+        <div class="col-xs-12 col-md-12 enk-offcanvas-content" id="content">
         </div>
-        <div class="col-md-3 collapsed" id="sidebarX">
+        <div class="col-xs-3 col-md-3 enk-offcanvas-sidebar enk-offcanvas-collapsed" id="sidebar">
+          <div id="p1" class="panel-hidden">p1</div>
+          <div id="p2" class="panel-hidden">p2</div>
+          <div id="p3" class="panel-hidden">p3</div>
         </div>
     </div>
-*/
 
-/* CSS
-<style>
-        #row-main {
-            overflow-x: hidden;
-        }
-
-        #contentX {
-            transition: width 0.3s ease;
-        }
-        
-        #sidebarX {
-            background: blue;
-            transition: margin 0.3s ease;
-       }
-
-        .collapsed {
-            margin-right: -25%;
-        }
-
-</style>
 */
 
 
-+function ($) {
-    "use strict";
++ function($) {
+  "use strict";
 
   // OFFCANVAS PUBLIC CLASS DEFINITION
   // =================================
 
-  var EnkOffCanvas = function (element, target, options) {
-      this.$element = $(element)
-      this.$target = target
-      this.options  = $.extend({}, EnkOffCanvas.DEFAULTS, options)
+  var EnkOffCanvas = function(element, target, options) {
+    this.$element = $(element)
+    this.$sidepanel = target
+    this.options = $.extend({}, EnkOffCanvas.DEFAULTS, options)
+
+    this.isOpen = false;
+    this.activePanel = '';
   }
 
   EnkOffCanvas.DEFAULTS = {
     toggle: true,
     autohide: true,
   }
-  
-  EnkOffCanvas.prototype.toggle = function () {
-    console.log('toggle');
-	
-	this.$target.toggleClass("collapsed");	
-	this.$element.toggleClass("col-md-12 col-md-9");
+
+  EnkOffCanvas.prototype.toggle = function(panel) {
+
+    var updateState = !this.isOpen;
+
+    if (panel !== this.activePanel) {
+
+      this.$sidepanel.children().addClass('enk-offcanvas-panel-hidden');
+      this.$sidepanel.children().removeClass('enk-offcanvas-panel-visible');
+
+      this.activePanel = panel;
+
+      this.$sidepanel.children(this.activePanel).addClass('enk-offcanvas-panel-visible');
+
+      updateState = true;
+    }
+
+    var changeState = this.isOpen != updateState;
+
+    this.isOpen = updateState;
+
+    // state change
+    if (changeState) {
+      this.$sidepanel.toggleClass("enk-offcanvas-collapsed");
+      this.$element.toggleClass("col-lg-12 col-lg-9");
+      this.$element.toggleClass("col-md-12 col-md-9");
+      this.$element.toggleClass("col-xs-12 col-xs-9");
+    }
   }
 
   // OFFCANVAS PLUGIN DEFINITION
@@ -64,15 +75,15 @@
 
   var old = $.fn.enkoffcanvas
 
-  $.fn.enkoffcanvas = function (option) {
-      return this.each(function () {
-          var $this = $(this)
-          var data = $this.data('bs.enkoffcanvas')
-          var options = $.extend({}, EnkOffCanvas.DEFAULTS, $this.data(), typeof option === 'object' && option)
+  $.fn.enkoffcanvas = function(option) {
+    return this.each(function() {
+      var $this = $(this)
+      var data = $this.data('bs.enkoffcanvas')
+      var options = $.extend({}, EnkOffCanvas.DEFAULTS, $this.data(), typeof option === 'object' && option)
 
-          if (!data) $this.data('bs.enkoffcanvas', (data = new EnkOffCanvas(this, $(option.source), options)))
-          if (typeof option === 'string') data[option]()
-      })
+      if (!data) $this.data('bs.enkoffcanvas', (data = new EnkOffCanvas(this, $(option.source), options)))
+      if (typeof option === 'string') data[option]()
+    })
   }
 
   $.fn.enkoffcanvas.Constructor = EnkOffCanvas
@@ -81,7 +92,7 @@
   // OFFCANVAS NO CONFLICT
   // ====================
 
-  $.fn.enkoffcanvas.noConflict = function () {
+  $.fn.enkoffcanvas.noConflict = function() {
     $.fn.enkoffcanvas = old
     return this
   }
@@ -89,22 +100,23 @@
   // OFFCANVAS DATA-API
   // =================
 
-  $(document).on('click.bs.enkoffcanvas.data-api', '[data-toggle=enkoffcanvas]', function (e) {
-    var $this   = $(this), href
-    var target  = $this.attr('data-target')
-        || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
+  $(document).on('click.bs.enkoffcanvas.data-api', '[data-toggle=enkoffcanvas]', function(e) {
+    var $this = $(this),
+      href
+    var target = $this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
     var $canvas = $(target)
-    var data    = $canvas.data('bs.enkoffcanvas')
-    var option  = data ? 'toggle' : $this.data()
+    var data = $canvas.data('bs.enkoffcanvas')
+    var option = data ? 'toggle' : $this.data()
 
     e.preventDefault();
     e.stopPropagation()
 
     if (!data) {
-	    $canvas.enkoffcanvas(option)
-	    data = $canvas.data('bs.enkoffcanvas')
+      $canvas.enkoffcanvas(option)
+      data = $canvas.data('bs.enkoffcanvas')
     }
-    data.toggle()
+
+    data.toggle($this.attr('data-canvas'));
   })
 
 }(window.jQuery);
